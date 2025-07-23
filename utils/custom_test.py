765a87753a7b33fa64f50cfb9e8638f1420dc0f2
pratiksinha35 <pratiksinha35@gmail.com>
2025-07-23 19:10:19 -0400

    padded input images to get 256x256

--- utils/custom_test.py
+++ utils/custom_test.py
@@ -1,6 +1,7 @@
 from torch.utils.data import Dataset
 import h5py
 import torch
+import torch.nn.functional as F
 
 class NWCSAFH5(Dataset):
     def __init__(self, h5_path, seq_len=4):
@@ -21,8 +22,13 @@ class NWCSAFH5(Dataset):
         # Shape: (4, 11, 252, 252)
         x = self.data[start:end]
         x = torch.tensor(x, dtype=torch.float32)
+
+        # Padding each image in the sequence to (256, 256)
+        # Current shape: (seq_len, channels, 252, 252)
+        # Pad format: (left, right, top, bottom) => (0, 4, 0, 4)
+
+        x = F.pad(x, pad=(0, 4, 0, 4), mode='constant', value=0)
         
         metadata = {"sequence_index": idx}
 
         return x, metadata
-
